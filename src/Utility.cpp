404a044bbea403a145e29ff8aa7a138a8ef3b85a
Natali <niribareva22@codingburgas.bg>
2025-01-23 10:09:09 +0200

    Implement Utility Functions

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -1,3 +1,24 @@
 //
 // Created by niribareva22 on 1/23/2025.
 //
+#include "Utility.h"
+#include <iostream>
+using namespace std;
+
+int getSortingChoice() {
+    int choice;
+    cout << "Choose sorting algorithm: 1) Selection Sort 2) Bubble Sort 3) Insertion Sort"<<endl;
+    cin >> choice;
+    return choice;
+}
+
+void sortStudents(vector<Student>& students, int choice) {
+    switch (choice) {
+        case 1: selectionSort(students); break;
+        case 2: bubbleSort(students); break;
+        case 3: insertionSort(students); break;
+        default:
+            cout << "Invalid choice."<<endl;
+            break;
+    }
+}
