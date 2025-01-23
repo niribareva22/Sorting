b062fe9c64d2a2b8a8ea88facf5bb41377794de0
Natali <niribareva22@codingburgas.bg>
2025-01-23 09:48:19 +0200

    Define Student Structure and Functions

--- include/Student.h
+++ include/Student.h
@@ -5,4 +5,15 @@
 #ifndef SORTING_STUDENT_H
 #define SORTING_STUDENT_H
 
+#include <string>
+#include <vector>
+
+struct Student {
+    std::string name;
+    double grade;
+};
+
+void createSampleStudents(std::vector<Student>& students);
+void printStudents(const std::vector<Student>& students);
+
 #endif //SORTING_STUDENT_H
