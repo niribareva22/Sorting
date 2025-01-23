45b4b6f5ce44777a4d316154df20cbe5027ddfd4
Natali <niribareva22@codingburgas.bg>
2025-01-23 09:59:37 +0200

    Define Utility Functions

--- include/Utility.h
+++ include/Utility.h
@@ -5,4 +5,10 @@
 #ifndef SORTING_UTILITY_H
 #define SORTING_UTILITY_H
 
+#include <vector>
+#include "Student.h"
+
+int getSortingChoice();
+void sortStudents(std::vector<Student>& students, int choice);
+
 #endif //SORTING_UTILITY_H
