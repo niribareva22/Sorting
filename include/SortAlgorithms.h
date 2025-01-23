c6e8b4e8438b1d38a3c6c30033577afd351ad33d
Natali <niribareva22@codingburgas.bg>
2025-01-23 09:51:10 +0200

    Define Sort Algorithms

--- include/SortAlgorithms.h
+++ include/SortAlgorithms.h
@@ -5,4 +5,11 @@
 #ifndef SORTING_SORTALGORITHMS_H
 #define SORTING_SORTALGORITHMS_H
 
+#include <vector>
+#include "Student.h"
+
+void selectionSort(std::vector<Student>& students);
+void bubbleSort(std::vector<Student>& students);
+void insertionSort(std::vector<Student>& students);
+
 #endif //SORTING_SORTALGORITHMS_H
